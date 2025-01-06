ce4fa140722e35a27d4ec003e955b8cf85cde250
Ahmadul Hasan <49359716+2018331109@users.noreply.github.com>
2025-01-06 21:46:37 +0600

    Hello 2025 round CodeForces

---
+++ Hello 2025/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+#define neg cout << -1 << endl
+#define pb push_back
+#define ff first
+#define ss second
+const int mod = 1e9+7;
+const int N = 200005;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    cout << max(n, m)+1 << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
